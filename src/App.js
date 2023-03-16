5d0dedb333dc591b78cfc52cdffd55ba83a0867e
luizfiorentino <lgfiorentino@gmail.com>
2023-03-16 17:46:07 +0100

    feat- all buttons added and displaying the correspondent advices

--- src/App.js
+++ src/App.js
@@ -4,24 +4,61 @@ import axios from "axios";
 
 function App() {
   const [field, setField] = useState("");
-  console.log("Field:", field);
+  const [arrayAdvices, setArrayAdvices] = useState([]);
+  //console.log("Field:", field);
 
-  const data = async () => {
+  const data = async (field) => {
     const response = await axios.get(
-      `https://api.adviceslip.com/advice/search/love`
+      `https://api.adviceslip.com/advice/search/${field}`
     );
     const advices = response.data.slips;
-    console.log("response", advices);
+    //console.log("response", advices);
+    setArrayAdvices(advices);
   };
 
   useEffect(() => {
-    data();
-  }, []);
+    field === "" ? <p>"Click a button"</p> : data(field);
+  }, [data(field)]);
+
+  //console.log("advices", arrayAdvices);
+
+  //cheese, love, friends, sleep, food, spiders, regret, work
+  const buttons = [
+    "cheese",
+    "love",
+    "friends",
+    "sleep",
+    "food",
+    "spiders",
+    "regret",
+    "work",
+  ];
 
   return (
     <div className="App">
       <h1>Welcome to Solid Advice!</h1>
-      <button onClick={() => setField("love")}>Love</button>
+      {buttons.map((button) => (
+        <button
+          key={button}
+          onClick={() => {
+            setField(button);
+
+            data(field);
+          }}
+        >
+          {button}
+        </button>
+      ))}
+
+      {field && <h3>Here's some solid advice 'bout {field}:</h3>}
+
+      <ol>
+        {arrayAdvices ? (
+          arrayAdvices.map((advice) => <li key={advice.id}>{advice.advice}</li>)
+        ) : (
+          <p>Loading</p>
+        )}
+      </ol>
     </div>
   );
 }
