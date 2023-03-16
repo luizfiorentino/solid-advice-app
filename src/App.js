e5f58832fdc7e1434d2f13f70f2eeb286caf34e6
luizfiorentino <lgfiorentino@gmail.com>
2023-03-16 17:02:01 +0100

    App created fetching data from the api via axios and useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
+  const [field, setField] = useState("");
+  console.log("Field:", field);
+
+  const data = async () => {
+    const response = await axios.get(
+      `https://api.adviceslip.com/advice/search/love`
+    );
+    const advices = response.data.slips;
+    console.log("response", advices);
+  };
+
+  useEffect(() => {
+    data();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to Solid Advice!</h1>
+      <button onClick={() => setField("love")}>Love</button>
     </div>
   );
 }
