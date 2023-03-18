f699a5abd0efd7bfb7815753026b5ed8680c6639
luizfiorentino <lgfiorentino@gmail.com>
2023-03-18 09:17:34 +0100

    useEffect working good

--- src/App.js
+++ src/App.js
@@ -5,24 +5,20 @@ import axios from "axios";
 function App() {
   const [field, setField] = useState("");
   const [arrayAdvices, setArrayAdvices] = useState([]);
-  //console.log("Field:", field);
-
-  const data = async (field) => {
-    const response = await axios.get(
-      `https://api.adviceslip.com/advice/search/${field}`
-    );
-    const advices = response.data.slips;
-    //console.log("response", advices);
-    setArrayAdvices(advices);
-  };
+  console.log("Field:", field);
 
   useEffect(() => {
-    field === "" ? <p>"Click a button"</p> : data(field);
-  }, [data(field)]);
+    const data = async () => {
+      const response = await axios.get(
+        `https://api.adviceslip.com/advice/search/${field}`
+      );
+      const advices = response.data.slips;
+      //console.log("response", advices);
+      setArrayAdvices(advices);
+    };
+    data();
+  }, [field]);
 
-  //console.log("advices", arrayAdvices);
-
-  //cheese, love, friends, sleep, food, spiders, regret, work
   const buttons = [
     "cheese",
     "love",
@@ -34,29 +30,31 @@ function App() {
     "work",
   ];
 
+  console.log("array advices", arrayAdvices);
+
   return (
     <div className="App">
       <h1>Welcome to Solid Advice!</h1>
-      {buttons.map((button) => (
-        <button
-          key={button}
-          onClick={() => {
-            setField(button);
-
-            data(field);
-          }}
-        >
-          {button}
-        </button>
-      ))}
-
+      <div className="buttons">
+        {buttons.map((button) => (
+          <button
+            className="btn"
+            key={button}
+            onClick={() => {
+              setField(button);
+            }}
+          >
+            {button}
+          </button>
+        ))}
+      </div>
       {field && <h3>Here's some solid advice 'bout {field}:</h3>}
 
       <ol>
-        {arrayAdvices ? (
+        {arrayAdvices !== [] ? (
           arrayAdvices.map((advice) => <li key={advice.id}>{advice.advice}</li>)
         ) : (
-          <p>Loading</p>
+          <li>Choose a field above to get some wise advice</li>
         )}
       </ol>
     </div>
